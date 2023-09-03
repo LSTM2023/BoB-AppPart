c2de4190c3202293df7e8c4e4058dcd34299c7ef
nute11a <87134443+seungha164@users.noreply.github.com>
2023-09-03 18:38:27 +0900

    :bug: 다국어처리 bug fix

--- BoB/lib/main.dart
+++ BoB/lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import './screens/Splash.dart';
-import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import './language.dart';
 import 'package:get/get.dart';
-import 'package:flutter_localization/flutter_localization.dart';
+import 'fcmSetting.dart';
 
-void main() {
-  KakaoSdk.init(nativeAppKey: '8e9bc1047c7add0a7a08665270df5693'); // 이 줄을 runApp 위에 추가한다.
+void main() async {
+  print('main');
+  WidgetsFlutterBinding.ensureInitialized();
+  String? firebaseToken = await fcmSetting();
   runApp(const MyApp());
 }
 
@@ -20,11 +21,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    return GetMaterialApp(
+
      translations: Languages(),
      locale: Get.deviceLocale,  // 기기에 설정한 언어
      fallbackLocale:  const Locale('ko','KR'),
+     title: 'Flutter Demo',
+     theme: ThemeData(
+       fontFamily: 'omew',
+     ),
      debugShowCheckedModeBanner: false,
-      //theme: ThemeData(),
       home: const Splash(),
     );
   }
